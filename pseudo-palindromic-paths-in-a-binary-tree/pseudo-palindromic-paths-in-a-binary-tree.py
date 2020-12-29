fd818001d1f60136bd7da8bc9985897e73800880
AmateurPirate <48704991+AmateurPirate@users.noreply.github.com>
2020-12-29 11:49:29 -0500

    Time: 340 ms (92.37%), Space: 50.1 MB (18.16%) - LeetHub

---
+++ pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pseudoPalindromicPaths (self, root: TreeNode) -> int:
+        cnt = 0
+        
+        def go(node, path, d):
+            if not node:
+                return
+            
+            d += 1
+            path ^= (1 << node.val)
+                
+            if node.left:
+                go(node.left, path, d)
+                
+            if node.right:
+                go(node.right, path, d)
+            
+            if not node.left and not node.right:
+                if not path or (d & 1 and math.log(path, 2) % 1 == 0):
+                    nonlocal cnt
+                    cnt += 1
+                
+        go(root, 0, 0)
+        
+        return cnt
