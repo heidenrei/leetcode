a91df7f35f673841d87e98b3ccb48d4654309cb0
Gavin <48704991+heidenrei@users.noreply.github.com>
2021-02-12 09:06:03 -0500

    Time: 28 ms (85.87%), Space: 14.2 MB (70.12%) - LeetHub

--- number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
+++ number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
@@ -1,19 +1,12 @@
 class Solution:
-    def numberOfSteps (self, n: int) -> int:
-        if n == 0:
-            return 0
-        cnt = 0
-        def go(n):
-            nonlocal cnt
-            if n == 1:
-                cnt += 1
-                return
-            
-            if not (n & 1):
-                cnt += 1
-                go(n//2)
-            else:
-                cnt += 2
-                go((n-1)//2)
-        go(n)
-        return cnt
+    def numberOfSteps (self, num: int) -> int:
+        steps = 0
+        
+        while num > 0:
+            if num & 1:
+                num -= 1
+            else:
+                num >>= 1
+            steps += 1
+            
+        return steps
