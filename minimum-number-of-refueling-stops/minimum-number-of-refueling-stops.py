e61cc47d1420c6f0c3cc502f3b86890dc906d7b4
G <48704991+heidenrei@users.noreply.github.com>
2021-06-12 21:28:47 -0400

    Time: 124 ms (60.63%), Space: 14.7 MB (36.91%) - LeetHub

---
+++ minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minRefuelStops(self, target, fuel, s):
+        heap = [] 
+        s = [(0, 0)] + s + [(target, 0)]
+        n, ans = len(s), 0
+        
+        for i in range(1, n):
+            fuel -= s[i][0] - s[i-1][0]
+            while heap and fuel < 0:
+                fuel -= heappop(heap)
+                ans += 1
+            if fuel < 0: return -1
+            heappush(heap, -s[i][1])
+            
+        return ans
