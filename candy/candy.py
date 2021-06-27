c074d6ac5cadd73942b4615c55e0418b33da5bd2
G <48704991+heidenrei@users.noreply.github.com>
2021-06-27 12:42:22 -0400

    Time: 452 ms (5.04%), Space: 18.5 MB (7.53%) - LeetHub

---
+++ candy/candy.py
@@ -0,0 +1,30 @@
+from sortedcontainers import SortedList
+
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        N = len(ratings)
+        
+        sl = SortedList()
+        
+        ans = [None for x in range(N)]
+        
+        for i, x in enumerate(ratings):
+            sl.add([x, i])
+            
+        while sl:
+            currx, curridx = sl.pop(0)
+            val = 1
+            if curridx - 1 >= 0 and ans[curridx - 1] != None:
+                if ratings[curridx] > ratings[curridx-1]:
+                    val = max(val, ans[curridx - 1]+1)
+                # elif ratings[curridx] == ratings[curridx-1]:
+                #     val = max(val, ans[curridx - 1])
+            if curridx + 1 < N and ans[curridx + 1] != None:
+                if ratings[curridx] > ratings[curridx+1]:
+                    val = max(val, ans[curridx +1]+1)
+                # elif ratings[curridx] == ratings[curridx+1]:
+                #     val = max(val, ans[curridx + 1])
+                
+            ans[curridx] = val
+        print(ans)
+        return sum(ans)
