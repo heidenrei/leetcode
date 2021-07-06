868bae937f4acefb4ac0da94f8a5ab30b8e70500
G <48704991+heidenrei@users.noreply.github.com>
2021-07-06 19:22:13 -0400

    Time: 24 ms (95.73%), Space: 14.4 MB (14.82%) - LeetHub

--- unique-binary-search-trees/unique-binary-search-trees.py
+++ unique-binary-search-trees/unique-binary-search-trees.py
@@ -1,17 +1,10 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        dp = [1]*(n+1)
-
-        # i is the number of nodes (and idx in dp array)
-        for i in range(2, n+1):
-            total = 0
-            # x is the root of bst
-            for x in range(1, i+1):
-                left = x - 1
-                right = i - x
-                total += dp[left] * dp[right]
-            dp[i] = total
         
-        return dp[n]
-                    
-                    
+        @functools.lru_cache(n)
+        def go(rem):
+            if rem == 0:
+                return 1
+            return sum(go(x) * go(rem-x-1) for x in range(rem))
+        
+        return go(n)
