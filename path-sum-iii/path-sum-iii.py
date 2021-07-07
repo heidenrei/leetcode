342d3632113e8c25d629d417c2128dd30fffa90d
G <48704991+heidenrei@users.noreply.github.com>
2021-07-07 02:41:35 -0400

    Time: 512 ms (31.57%), Space: 128.2 MB (5.82%) - LeetHub

---
+++ path-sum-iii/path-sum-iii.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: TreeNode, targetSum: int) -> int:
+        if not root:
+            return 0
+        ans = 0
+        def go(node, pfs, d):
+            tmp = defaultdict(int)
+            for k, v in d:
+                tmp[k] = v
+            d = tmp
+            curr = pfs + node.val
+            
+            nonlocal ans
+            ans += d[curr - targetSum]
+            d[curr] += 1
+            d = tuple([(k, v) for k, v in d.items()])
+            
+            if node.left:
+                go(node.left, curr, d)
+            if node.right:
+                go(node.right, curr, d)
+            
+        go(root, 0, tuple([(0, 1)]))
+        
+        return ans
+            
