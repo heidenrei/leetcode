1d734d2df695ddf12a73d9f117d2900c29e43a01
G <48704991+heidenrei@users.noreply.github.com>
2021-07-16 14:42:22 -0400

    Time: 4104 ms (5.02%), Space: 14.9 MB (9.07%) - LeetHub

---
+++ ugly-number-ii/ugly-number-ii.py
@@ -0,0 +1,5 @@
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        ugly = sorted([2**a * 3**b * 5**c for a in range(32) for b in range(20) for c in range(14)])
+        
+        return ugly[n-1]
