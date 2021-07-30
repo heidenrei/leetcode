b0b4b5897e3d501c23be1b79aa67617bd9cb5cf5
G <48704991+heidenrei@users.noreply.github.com>
2021-07-30 15:14:18 -0400

    Time: 36 ms (38.96%), Space: 14.4 MB (56.87%) - LeetHub

--- map-sum-pairs/map-sum-pairs.py
+++ map-sum-pairs/map-sum-pairs.py
@@ -1,48 +1,43 @@
 class Node:
     def __init__(self):
+        self.val = 0
         self.children = {}
-        self.val_sum = 0
-
+        
 class Trie:
     def __init__(self):
         self.root = Node()
+        self.d = defaultdict(int)
         
-    def insert(self, word, new_val, old_val):
+    def insert(self, val, key):
         curr = self.root
-        for ch in word:
+        
+        for ch in key:
             if ch not in curr.children:
                 curr.children[ch] = Node()
+                
             curr = curr.children[ch]
-            curr.val_sum += new_val - old_val
-        
-    def search(self, word):
+            curr.val += val
+            curr.val -= self.d[key]
+            
+        self.d[key] = val
+            
+    def query(self, prefix):
         curr = self.root
-        for ch in word:
-            if ch in curr.children:
-                curr = curr.children[ch]
-            else:
+        
+        for ch in prefix:
+            if ch not in curr.children:
                 return 0
-        return curr.val_sum
+            else:
+                curr = curr.children[ch]
+                
+        return curr.val
 
 class MapSum:
-
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.t = Trie()
-        self.d = defaultdict(int)
 
     def insert(self, key: str, val: int) -> None:
-        old_val = self.d[key]
-        self.t.insert(key, val, old_val)
-        self.d[key] = val
+        self.t.insert(val, key)
 
     def sum(self, prefix: str) -> int:
-        return self.t.search(prefix)
-
-
-# Your MapSum object will be instantiated and called as such:
-# obj = MapSum()
-# obj.insert(key,val)
-# param_2 = obj.sum(prefix)
+        return self.t.query(prefix)
