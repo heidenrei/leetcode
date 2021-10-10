777e8a9c2c12a98832e25f37be26e22e3db498a0
G <48704991+heidenrei@users.noreply.github.com>
2021-10-10 02:43:34 -0400

    Time: 48 ms (96.67%), Space: 14.4 MB (23.30%) - LeetHub

--- bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
+++ bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
@@ -2,9 +2,15 @@ class Solution:
     def rangeBitwiseAnd(self, m: int, n: int) -> int:
         cnt = 0
         while m != n:
+            # print(bin(m))
+            # print(bin(n))
             n //= 2
             m //= 2
             cnt += 1
-            
+        
+        # print(bin(m))
+        # print(bin(n))
+        # print()
+        # print(cnt)
         return m << cnt
             
