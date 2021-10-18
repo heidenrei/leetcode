bc4d29bc940bea664b0d7f26dd27da785ab811ea
G <48704991+heidenrei@users.noreply.github.com>
2021-10-18 12:15:29 -0400

    Time: 40 ms (34.83%), Space: 14.4 MB (9.47%) - LeetHub

--- cousins-in-binary-tree/cousins-in-binary-tree.py
+++ cousins-in-binary-tree/cousins-in-binary-tree.py
@@ -1,27 +1,29 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
-        d = defaultdict(list)
-        
-        def go(node, prev, level):
-            if not node:
-                return
-            
-            nonlocal d
-            d[node.val] = [prev, level]
-            
-            if node.left:
-                go(node.left, node.val, level+1)
-            if node.right:
-                go(node.right, node.val, level+1)
-                
-        go(root, None, 0)
-        
-        return d[x][0] != d[y][0] and d[x][1] == d[y][1]
-                
-        
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        d = defaultdict(list)
+        p = defaultdict(int)
+        
+        q = [root]
+        level = 0
+        
+        while q:
+            tmp = []
+            while q:
+                node = q.pop()
+                d[node.val].append(level)
+                if node.left:
+                    p[node.left.val] = node.val
+                    tmp.append(node.left)
+                if node.right:
+                    p[node.right.val] = node.val
+                    tmp.append(node.right)
+            q = tmp
+            level += 1
+        
+        return p[x] != p[y] and d[x] == d[y]
