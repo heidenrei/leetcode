ce220545ba7a76d119d7d5611ce4838687e3d2ec
G <48704991+heidenrei@users.noreply.github.com>
2022-03-01 16:27:02 -0500

    Time: 75 ms (97.09%), Space: 20.8 MB (51.42%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,6 +1,6 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = [0 for x in range(n+1)]
+        ans = [0] * (n+1)
         for x in range(n+1):
             ans[x] = x.bit_count()
             
