827f2b7f3e80431a7bd7dc408de05545faee3232
G <48704991+heidenrei@users.noreply.github.com>
2022-03-04 01:09:01 -0500

    Time: 816 ms (5.34%), Space: 14.2 MB (46.60%) - LeetHub

---
+++ 799-champagne-tower/799-champagne-tower.py
@@ -0,0 +1,24 @@
+class Solution:
+    def champagneTower(self, poured: int, row: int, col: int) -> float:
+        if poured == 0:
+            return 0
+        m = 200
+        dp = [[0 for x in range(m)] for y in range(m)]
+        if row == 0 and col == 0:
+            return min(1, poured)
+        dp[0][0] = poured - 1
+        for i in range(1, m):
+            for j in range(i+1):
+                if j > 0:
+                    dp[i][j] = dp[i-1][j-1]/2
+                dp[i][j] += dp[i-1][j]/2
+                if i == row and j == col:
+                    # for x in dp:
+                    #     print(x)
+                    return min(1, dp[i][j])
+                dp[i][j] = max(0, dp[i][j]-1)
+        
+#         for x in dp:
+#             print(x)
+                       
+#         return dp[row][col]
