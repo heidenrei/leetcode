a49c39eaff56d0e969192df5bcc25d88fff43123
G <48704991+heidenrei@users.noreply.github.com>
2022-03-07 10:51:48 -0500

    Time: 1543 ms (48.48%), Space: 19.2 MB (83.98%) - LeetHub

---
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxSideLength(self, A, k):
+        R, C = len(A), len(A[0])
+        pfsA = []
+        for x in A:
+            pfsA.append(list(accumulate(x, initial=0)))
+        C += 1
+        def is_good(x):
+            for j in range(x, C):
+                tmp = 0
+                for i in range(x):
+                    tmp += pfsA[i][j] - pfsA[i][j-x]
+                if tmp <= k:
+                    return True
+                for i in range(x, R):
+                    tmp += pfsA[i][j] - pfsA[i][j-x]
+                    tmp -= pfsA[i-x][j] - pfsA[i-x][j-x]
+                    if tmp <= k:
+                        return True
+                    
+            return False
+        
+        print(is_good(1))
+        print(is_good(2))
+        
+        l = 0
+        r = min(R, C-1)
+        while l < r:
+            m = l + r >> 1
+            if is_good(m):
+                l = m + 1
+            else:
+                r = m
+        if is_good(l):
+            return l
+        return l - 1
