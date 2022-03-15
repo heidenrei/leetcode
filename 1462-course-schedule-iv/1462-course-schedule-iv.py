95163504e3aaaef5f15fd7ea628498ac790843fc
G <48704991+heidenrei@users.noreply.github.com>
2022-03-14 21:41:03 -0400

    Time: 1160 ms (67.52%), Space: 17.9 MB (32.26%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -3,10 +3,8 @@ class Solution:
         d = defaultdict(list)
         for x, y in p:
             d[y].append(x)
-        print(d)
         pres = defaultdict(set)
         
-        #@cache
         def go(node):
             ans = set()
             for nei in d[node]:
@@ -20,7 +18,6 @@ class Solution:
         for x in range(n):
             seen = set([x])
             pres[x] = go(x)
-        print(pres)
         ans = []
         for x, y in queries:
             ans.append(x in pres[y])
