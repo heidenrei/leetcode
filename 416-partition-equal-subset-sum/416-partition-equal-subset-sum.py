b9e271bb0fdb27af4c9961a4084a05c18bbb05fb
G <48704991+heidenrei@users.noreply.github.com>
2022-03-16 23:39:08 -0400

    Time: 2576 ms (32.86%), Space: 342.8 MB (5.02%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,13 +1,10 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         N = len(nums)
-        k = sum(nums)/2
-        if k % 1 != 0:
+        if sum(nums) & 1:
             return False
         @cache
         def go(i, d):
-            if i == N:
-                return d == 0
-            return go(i+1, d+nums[i]) or go(i+1, d-nums[i])
+            return d == 0 if i == N else go(i+1, d+nums[i]) or go(i+1, d-nums[i])
         
         return go(0, 0)
