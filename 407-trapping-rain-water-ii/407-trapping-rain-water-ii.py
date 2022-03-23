6ffe349c8ef6d92dc663956fd3ddfc0ec09ecb36
G <48704991+heidenrei@users.noreply.github.com>
2022-03-23 13:56:36 -0400

    Time: 534 ms (62.53%), Space: 20.9 MB (9.87%) - LeetHub

--- 407-trapping-rain-water-ii/407-trapping-rain-water-ii.py
+++ 407-trapping-rain-water-ii/407-trapping-rain-water-ii.py
@@ -1,5 +1,3 @@
-#from sortedcontainers import SortedList
-
 class Solution:
     def trapRainWater(self, A: List[List[int]]) -> int:
         R, C = len(A), len(A[0])
@@ -7,19 +5,18 @@ class Solution:
             
         walls = {(A[i][0], i, 0) for i in range(R)} | {(A[i][C-1], i, C-1) for i in range(R)} | {(A[0][j], 0, j) for j in range(C)} | {(A[R-1][j], R-1, j) for j in range(C)}
         seen = {(i, 0) for i in range(R)} | {(i, C-1) for i in range(R)} | {(0, j) for j in range(C)} | {(R-1, j) for j in range(C)}
-        #pq = SortedList(walls, key=lambda x: x[2])
         pq = list(walls);heapify(pq)
         level = 0
         ans = 0
+        
         while pq:
             h, i, j = heappop(pq)
             level = max(h, level)
             for di, dj in DIRS:
                 ni, nj = di + i, dj + j
                 if 0 <= ni < R and 0 <= nj < C and (ni, nj) not in seen:
-                    if level > A[ni][nj]:
-                        ans += level - A[ni][nj]
-                    #pq.add((ni, nj, A[ni][nj]))
+                    #if level > A[ni][nj]:
+                    ans += max(level, A[ni][nj]) - A[ni][nj]
                     heappush(pq, (A[ni][nj], ni, nj))
                     seen.add((ni, nj))
                     
