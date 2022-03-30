9d52d478107441f5430d268068cc10e5c228a22d
G <48704991+heidenrei@users.noreply.github.com>
2022-03-30 01:34:55 -0400

    Time: 54 ms (85.38%), Space: 14.4 MB (54.55%) - LeetHub

--- 1000-minimum-cost-to-merge-stones/1000-minimum-cost-to-merge-stones.py
+++ 1000-minimum-cost-to-merge-stones/1000-minimum-cost-to-merge-stones.py
@@ -1,13 +1,13 @@
 class Solution:
     def mergeStones(self, A: List[int], k: int) -> int:
-        N = len(A)
-        if (N-1) % (k-1) != 0:
+        if (len(A)-1) % (k-1) != 0:
             return -1
         pfs = list(accumulate(A, initial=0))
-
         @cache
-        def go(i, j):
-            if j - i + 1 < k:
+        def go(i, j): # cost to merge items i to j []
+            # if we can't merge them return 0
+            N = j - i + 1
+            if N < k != 0:
                 return 0
             ans = inf
             for m in range(i, j, k-1):
@@ -15,8 +15,8 @@ class Solution:
                 if tmp < ans:
                     ans = tmp
                     
-            #res = min(go(i, mid) + go(mid + 1, j) for mid in range(i, j, k - 1))
-            if (j - i) % (k - 1) == 0:
-                ans += pfs[j + 1] - pfs[i]
+            if (N-1) % (k-1) == 0:
+                ans += pfs[j+1] - pfs[i]
             return ans
-        return go(0, N - 1)
+        
+        return go(0, len(A)-1)
