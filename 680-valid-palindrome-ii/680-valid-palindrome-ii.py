acd6f5a69071c1ba3673efe4f66e9b7bc3a2fb50
G <48704991+heidenrei@users.noreply.github.com>
2022-04-02 00:25:40 -0400

    Time: 166 ms (62.31%), Space: 14.6 MB (52.23%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,7 +1,5 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        if s == s[::-1]:
-            return True
         N = len(s)
         for i in range(N//2):
             if s[i] != s[N-i-1]:
@@ -9,4 +7,4 @@ class Solution:
                 del_right = s[:N-i-1] + s[N-i:]
                 return del_left == del_left[::-1] or del_right == del_right[::-1]
             
-        return False
+        return True
