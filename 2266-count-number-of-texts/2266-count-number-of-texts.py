5330493d628324ce524006363b2cb15179e38742
G <48704991+heidenrei@users.noreply.github.com>
2022-05-16 02:07:43 -0400

    Time: 1096 ms (57.04%), Space: 18.8 MB (85.85%) - LeetHub

--- 2266-count-number-of-texts/2266-count-number-of-texts.py
+++ 2266-count-number-of-texts/2266-count-number-of-texts.py
@@ -1,27 +1,26 @@
 class Solution:
     def countTexts(self, s: str) -> int:
         MOD = 10**9+7
+        s = '0' + s
         N = len(s)
-        @cache
-        def go(i):
-            if i == N:
-                return 1
-            ans = 0
-            if s[i] in '79':
-                for j in range(4):
-                    if i+j == N or s[i] != s[i+j]:
-                        break
-                    else:
-                        ans += go(i+j+1)
-                        ans %= MOD
-            else:
-                for j in range(3):
-                    if i+j == N or s[i] != s[i+j]:
-                        break
-                    else:
-                        ans += go(i+j+1)
-                        ans %= MOD
-                        
-            return ans
+        dp = [0]*N
+        dp[0] = 1
         
-        return go(0)
+        for i in range(1, N):
+            # if s[i] != s[i-1]:
+            #     dp[i] = dp[i-1]
+            if s[i] in '79':
+                for j in range(1,5):
+                    dp[i] += dp[i-j]
+                    dp[i] %= MOD
+                    if s[i] != s[i-j]:
+                        break
+
+            else:
+                for j in range(1, 4):
+                    dp[i] += dp[i-j]
+                    dp[i] %= MOD
+                    if s[i] != s[i-j]:
+                        break
+                
+        return dp[-1]
