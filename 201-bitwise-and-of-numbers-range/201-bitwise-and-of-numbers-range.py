7e4fed63c17201e0f5a1a13c44b2c8c3bf57cbb8
G <48704991+heidenrei@users.noreply.github.com>
2022-07-30 22:09:06 -0400

    Time: 106 ms (35.38%), Space: 13.9 MB (68.09%) - LeetHub

--- 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.py
+++ 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.py
@@ -6,7 +6,6 @@ class Solution:
         for i in range(32, 0, -1):
             if l & (1<<i) and r & (1<<i):
                 tmp = ((1<<i) - 1) + ((r>>(i+1))<<(i+1))
-                print(tmp)
                 if tmp < l:
                     ans |= 1<<i
                 else:
