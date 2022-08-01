4d4d533aa6d4301cb7eb702e230b99bf416f2a1e
G <48704991+heidenrei@users.noreply.github.com>
2022-07-31 20:33:19 -0400

    Time: 53 ms (40.65%), Space: 14.3 MB (6.77%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,10 +1,15 @@
 class Solution:
-    def uniquePaths(self, R: int, C: int) -> int:
+    def uniquePaths(self, m: int, n: int) -> int:
         @cache
         def go(i, j):
-            if i == R-1 and j == C-1:
+            if i == m - 1and j == n - 1:
                 return 1
-            if i == R or j == C:
-                return 0
-            return go(i+1, j) + go(i, j+1)
-        return go(0,0)
+            ans = 0
+            for di, dj in [[0,1],[1,0]]:
+                ni, nj = di + i, dj + j
+                if 0 <= ni < m and 0 <= nj < n:
+                    ans += go(ni, nj)
+                    
+            return ans
+        
+        return go(0, 0)
