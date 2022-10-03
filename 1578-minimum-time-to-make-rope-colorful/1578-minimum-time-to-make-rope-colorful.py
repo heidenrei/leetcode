ede21ee4ee5dc8999e43a2e59d6af2d80d9c1160
G <48704991+heidenrei@users.noreply.github.com>
2022-10-03 18:19:34 -0400

    Time: 3497 ms (5.12%), Space: 25.2 MB (12.73%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        N = len(colors)
+        ans = 0
+        curr = colors[0]
+        starti = 0
+        for i, x in enumerate(colors[1:]):
+            i += 1
+            if x != curr:
+                if starti is not None:
+                    tmp = sorted(neededTime[starti:i])
+                    ans += sum(tmp[:-1])
+                    print(curr, tmp)
+                    curr = x
+                    starti = i
+        if curr is not None:
+            tmp = sorted(neededTime[starti:i+1])
+            ans += sum(tmp[:-1])
+            print(curr, tmp)
+        return ans
+                    
