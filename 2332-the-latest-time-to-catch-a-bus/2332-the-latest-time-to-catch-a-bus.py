31bf9284a3dce792c427cc35d8d75d2d830829f1
G <48704991+heidenrei@users.noreply.github.com>
2022-10-17 21:24:05 -0400

    Time: 5223 ms (5.10%), Space: 81.5 MB (5.45%) - LeetHub

---
+++ 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.py
@@ -0,0 +1,39 @@
+class Solution:
+    def latestTimeCatchTheBus(self, buses: List[int], passengers: List[int], cap: int) -> int:
+        events = []
+        for x in buses:
+            events.append([x, 1])
+        for x in passengers:
+            events.append([x, 0])
+        used = set(passengers)
+        free = set()
+        for x in used:
+            if x-1 not in used:
+                free.add(x-1)
+        for x in buses:
+            if x not in used:
+                free.add(x)
+        print(free)
+        for x in free:
+            events.append([x, 0])
+        
+        events.sort()
+        # need to be last person on last bus
+        q = []
+        max_free = 0
+        for x, t in events:
+            if t == 1:
+                #print(x, q)
+                #max_free = 0
+                cnt = 0
+                while q and cnt < cap:
+                    tmp = heappop(q)
+                    if tmp in used:
+                        cnt += 1
+                    else:
+                        #print('111', tmp)
+                        max_free = tmp
+            else:
+                heappush(q, x)
+        return max_free
+                    
