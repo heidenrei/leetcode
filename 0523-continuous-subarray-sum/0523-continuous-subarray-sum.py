e613d1d3e6b57efe94dc1fb4eb67a58cf7bf5ded
G <48704991+heidenrei@users.noreply.github.com>
2022-10-25 20:29:00 -0400

    Time: 1771 ms (65.26%), Space: 28.7 MB (97.51%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        N = len(nums)
+        seen = set()
+        
+        curr = 0
+        step = 0
+        
+        for x in nums:
+            curr = (curr+x) % k
+            if curr in seen:
+                return True
+            seen.add(step)
+            step = curr
+            
+        return False
+        
