d71f5e023e4aa132e0867ca1ba1a3a2e20ab3822
G <48704991+heidenrei@users.noreply.github.com>
2023-01-08 15:48:26 -0500

    Time: 596 ms (74.93%), Space: 28.4 MB (35.36%) - LeetHub

--- 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.py
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.py
@@ -10,35 +10,3 @@ class Solution:
         
         pfs = list(accumulate(damage))
         return max(pfs) + 1
-        
-#         @cache
-#         def is_good(i, x):
-#             if i == N:
-#                 return True
-#             if x - damage[i] > 0 and is_good(i+1, x - damage[i]):
-#                     return True
-#             # elif x - max(0, damage[i] - armor) > 0 and not used:
-#             #     if is_good(i+1, x - max(damage[i] - armor, 0), True):
-#             #         return True
-#             #     else:
-#             #         return False
-#             else:
-#                 return False
-        
-# #         for x in range(8, 13):
-# #             print(x, is_good(0, x, 0))
-        
-# #         return 0
-
-#         def is_good(x):
-            
-        
-#         l, r = 0, sum(damage)
-#         while l <= r:
-#             m = l + r >> 1
-#             if is_good(0, m):
-#                 r = m - 1
-#             else:
-#                 l = m + 1
-                
-#         return r + 1
