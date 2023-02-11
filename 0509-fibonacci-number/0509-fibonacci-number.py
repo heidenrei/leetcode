5c15f05035140b3cdcd64befd68001d5c4092a61
G <48704991+heidenrei@users.noreply.github.com>
2023-02-10 21:24:05 -0500

    Time: 383 ms (31.58%), Space: 13.9 MB (45.15%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -2,9 +2,9 @@ class Solution:
     def fib(self, n: int) -> int:
         if not n:
             return 0
-        @cache
-        def go(x):
-            if x < 3:
+        
+        def go(n):
+            if n < 3:
                 return 1
-            return go(x-1) + go(x-2)
+            return go(n-1) + go(n-2)
         return go(n)
