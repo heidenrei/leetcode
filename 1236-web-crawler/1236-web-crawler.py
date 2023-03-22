19c5470dc8d0c221ba4c71e003d23664c69d2682
G <48704991+heidenrei@users.noreply.github.com>
2023-03-22 01:34:31 -0400

    Time: 208 ms (87.53%), Space: 20.5 MB (96.51%) - LeetHub

--- 1236-web-crawler/1236-web-crawler.py
+++ 1236-web-crawler/1236-web-crawler.py
@@ -15,25 +15,11 @@ class Solution:
         start = startUrl[7:].split('/')[0]
         def go(startUrl, htmlParser, starter):
             s.add(startUrl)
-
-            #start = startUrl.split('.com')[0]
-            #print(startUrl)
             for x in htmlParser.getUrls(startUrl):
                 tmp_starter = x[7:].split('/')[0]
                 if tmp_starter != starter:
                     continue
                 if x not in s:
                     go(x, htmlParser, starter)
-                # x = x[7:]
-                # x = x.split('/')
-                # start = x[0]
-                # end = x[1:]
-                # for i in range(len(end)):
-                #     curr = 'http://' + start + '/' + '/'.join(end[:i])
-                #     if curr[-1] == '/':
-                #         curr = curr[:-1]
-                #     if curr not in s and curr + '/' not in s:
-                #         #print(curr)
-                #         go(curr, htmlParser, starter)
         go(startUrl, htmlParser, start)
         return list(s)
