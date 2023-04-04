41768ae60f92ea9561ccd9cfd593df5d696382d8
G <48704991+heidenrei@users.noreply.github.com>
2023-04-04 00:30:09 -0400

    Time: 10 ms (40.91%), Space: 10.6 MB (11.36%) - LeetHub

--- 0484-find-permutation/0484-find-permutation.cpp
+++ 0484-find-permutation/0484-find-permutation.cpp
@@ -1,13 +1,22 @@
-class Solution:
-    def findPermutation(self, s: str) -> List[int]:
-        ans = []
-        s += 'I'
-        stack = []
-        for i, x in enumerate(s):
-            if x == 'D':
-                stack.append(i+1)
-            else:
-                ans.append(i+1)
-                while stack:
-                    ans.append(stack.pop())
-        return ans
+class Solution {
+public:
+    vector<int> findPermutation(string s) {
+        vector<int> ans, stack;
+        s.push_back('I');
+        int n = s.length();
+        for (int i=0;i<n;i++) {
+            if (s[i]=='D') {
+                stack.push_back(i+1);
+            }
+            else {
+                ans.push_back(i+1);
+                while (!stack.empty()) {
+                    int u = stack.back();
+                    stack.pop_back();
+                    ans.push_back(u);
+                }
+            }
+        }
+        return ans;
+    }
+};
