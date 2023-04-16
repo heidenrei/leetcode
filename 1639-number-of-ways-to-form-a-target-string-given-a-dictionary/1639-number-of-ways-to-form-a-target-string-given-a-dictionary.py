d1e3041ccc5d74f805e97740faae981552250d93
G <48704991+heidenrei@users.noreply.github.com>
2023-04-16 09:23:04 -0400

    Time: 1663 ms (75.10%), Space: 20.4 MB (99.22%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.py
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.py
@@ -1,9 +1,19 @@
 class Solution:
     def numWays(self, words: List[str], target: str) -> int:
-        n, mod = len(target), 10**9 + 7
-        res = [1] + [0] * n
+        # n, mod = len(target), 10**9 + 7
+        # res = [1] + [0] * n
+        # for i in range(len(words[0])):
+        #     count = Counter(w[i] for w in words)
+        #     for j in range(n - 1, -1, -1):
+        #         res[j + 1] += res[j] * count[target[j]] % mod
+        # return res[n] % mod
+    
+        N = len(target)
+        MOD = 10**9+7
+        ans = [1] + [0]*N
         for i in range(len(words[0])):
-            count = Counter(w[i] for w in words)
-            for j in range(n - 1, -1, -1):
-                res[j + 1] += res[j] * count[target[j]] % mod
-        return res[n] % mod
+            cnt = Counter(w[i] for w in words)
+            for j in range(N-1, -1, -1):
+                ans[j+1] += ans[j] * cnt[target[j]]
+                ans[j+1] %= MOD
+        return ans[N]
