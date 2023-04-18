2483be09c72fccb697ab58a2b85ecd50caffa794
G <48704991+heidenrei@users.noreply.github.com>
2023-04-18 17:52:22 -0400

    Time: 292 ms (12.28%), Space: 13.8 MB (73.68%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
@@ -1,15 +1,17 @@
 class Solution:
     def distinctSubseqII(self, s: str) -> int:
         MOD = 10**9+7
-        
+        N = len(s)
         d = defaultdict(int)
         
         for x in s:
-            d[x] += 1
+            d[x] += 1# + N - d[x]
+            #d[x] += N
+            #d[x] -= d[x]
             for k in d.keys():
                 if k != x:
                     d[x] += d[k]
                     d[x] %= MOD
 
             
-        return sum(d[x] for x in d.keys()) % MOD
+        return sum(v for v in d.values()) % MOD
