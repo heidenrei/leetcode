2241e1b769615a0129ff920780a9e0eb7ce37fdf
G <48704991+heidenrei@users.noreply.github.com>
2023-05-02 19:02:13 -0400

    Time: 1565 ms (80.55%), Space: 0B (100.00%) - LeetHub

---
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+select user_id
+    , concat(
+        upper(substring(name,1,1)), lower(substring(name,2,len(name)))
+    ) name
+from Users
+order by user_id
