c6504f4fd4a7b3d1e3a8a88157b47e1f98feb243
G <48704991+heidenrei@users.noreply.github.com>
2023-05-29 15:48:58 -0400

    Time: 3902 ms (20.11%), Space: 30.9 MB (50.04%) - LeetHub

--- 2680-maximum-or/2680-maximum-or.py
+++ 2680-maximum-or/2680-maximum-or.py
@@ -2,10 +2,6 @@ class Solution:
     def maximumOr(self, nums: List[int], k: int) -> int:
         ans = 0
         d = defaultdict(int)
-        
-        #print(bin(94)[2:].zfill(8))
-        #print(bin(35)[2:].zfill(8))
-        #print((94<<k) | 35)
         for x in nums:
             ans |= x
             for i in range(32):
@@ -13,11 +9,9 @@ class Solution:
                     d[i] += 1
         best = 0            
         for x in nums:
-            tmp = ans# | (x<<k)
-            #print(x<<k, tmp)
+            tmp = ans
             for i in range(32):
                 if x & (1<<i) and d[i] == 1:
-                    #print(i)
                     tmp ^= 1<<i
             tmp |= (x<<k)
             
