4ae1e3efed1191462d3146e64467eb3524a8cf31
G <48704991+heidenrei@users.noreply.github.com>
2023-09-15 20:43:42 -0400

    Time: 1307 ms (29.14%), Space: 30.3 MB (5.05%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        R, C = len(heights), len(heights[0])
+        
+        INF = float('inf')
+        
+        DIRS = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        
+        def is_possible(limit):
+            seen = set(tuple([0, 0]))
+            def go(i, j):
+                if i == R - 1 and j == C - 1:
+                    return True
+                for di, dj in DIRS:
+                    ni, nj = i + di, j + dj
+                    if 0 <= ni < R and 0 <= nj < C and tuple([ni, nj]) not in seen and abs(heights[ni][nj] - heights[i][j]) <= limit:
+                        seen.add(tuple([i, j]))
+                        if go(ni, nj):
+                            return True
+                        
+            return go(0, 0)
+        
+        l = 0
+        r = max([max(x) for x in heights])
+                
+        while l < r:
+            m = (l+r)>>1
+            if is_possible(m):
+                r = m
+            else:
+                l = m + 1
+                
+        return l
