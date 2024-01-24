aa9676a202b278773045b8be222c90febf8fc4a4
G <48704991+heidenrei@users.noreply.github.com>
2024-01-24 09:11:17 -0500

    Time: 329 ms (98.67%), Space: 43.6 MB (97.33%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -0,0 +1,21 @@
+class Solution:
+    def pseudoPalindromicPaths (self, root: TreeNode) -> int:
+        cnt = 0
+        
+        def go(node, path, d):
+            d += 1
+            path ^= (1 << node.val)
+                
+            if node.left:
+                go(node.left, path, d)
+                
+            if node.right:
+                go(node.right, path, d)
+            
+            if not node.left and not node.right:
+                nonlocal cnt
+                cnt += not path or (d & 1 and math.log(path, 2) % 1 == 0)
+                
+        go(root, 0, 0)
+        
+        return cnt
